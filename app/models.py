f7562365258c719e96c6c206d6b3d605601701c3
pbv615@alumni.ku.dk <pbv615@alumni.ku.dk>
2022-03-27 13:26:32 +0200

    Implemented user login check

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
@@ -21,3 +26,12 @@ class Post(db.Model):
     
     def __repr__(self):
         return f"User('{self.title}', '{self.date_posted}')"
+
+def validate_input(input_data):
+    username = User.query.filter_by(username = input_data['username']).first()
+    email = User.query.filter_by(email = input_data['email']).first()
+    if username:
+        return('Username already exists.')
+    elif email:
+        return ('Email already used.')
+
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app.models import User, Post
+from app.models import User, Post, validate_input
 from app import app, db, bcrypt
 from flask import request, jsonify
 
@@ -10,6 +10,10 @@ def home():
 def register():
     if request.method == 'POST':
         request_data = request.form
+        user_valid = validate_input(request_data)
+        if user_valid:
+            return user_valid
+
         hashed_pw = bcrypt.generate_password_hash(request_data['password'])
         user = User(username = request_data['username'], 
                     email = request_data['email'], 
@@ -18,3 +22,11 @@ def register():
         db.session.commit()
 
     return f'Database: {User.query.all()}'
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login_data = request.form
+        user = User.query.filter_by(email = login_data['email']).first()
+        if user and bcrypt.check_password_hash(user.password, login_data['password']):
+            return f'Database: {User.query.all()}'
