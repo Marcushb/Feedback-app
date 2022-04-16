9c0b3ee3a32d5f9143c8c76fe86731f642bfafd5
pbv615@alumni.ku.dk <pbv615@alumni.ku.dk>
2022-04-16 13:10:42 +0200

    Multiple changes to basic structure

--- app/models.py
+++ app/models.py
@@ -1,48 +1,70 @@
-from app import db, login_manager
+from sys import settrace
+from app import db, login_manager, bcrypt
 from datetime import datetime
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(60), nullable = False)
-    posts = db.relationship('Post', backref = 'user_posts', lazy = True)
-    user_answers = db.relationship('Answer', backref = 'user_answers', lazy = True)
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    user_posts = db.relationship(
+        'Post',
+        foreign_keys='Post.posted_by_id',
+        backref='user_posts',
+        lazy=True)
+    user_answers = db.relationship(
+        'Answer',
+        foreign_keys='Answer.answered_by_id',
+        backref='user_answers',
+        lazy=True)
+
+    @property
+    def unhashed_password(self):
+        raise AttributeError('Cannot view unhashed password')
+
+    @unhashed_password.setter
+    def unhashed_password(self, unhashed_password):
+        self.password = bcrypt.generate_password_hash(unhashed_password)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}', '{self.email}', '{self.password}')"
 
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(100), nullable = False)
-    date_posted = db.Column(db.DateTime, default = datetime.utcnow())
-    content = db.Column(db.Text, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    post_answers = db.relationship('Answer', backref = 'post_answers', lazy = True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    content = db.Column(db.Text, nullable=False)
+    posted_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_answers = db.relationship('Answer', backref='post_answers', lazy=True)
+
     def __repr__(self):
         return f"Post('{self.title}', '{self.content}')"
 
+
 class Answer(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    date_posted = db.Column(db.DateTime, default = datetime.utcnow())
-    content = db.Column(db.Text, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    content = db.Column(db.Text, nullable=False)
+    answered_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    posted_by_id = db.Column(db.Integer, db.ForeignKey('post.id'))
 
     def __repr__(self):
         return f"Answer('{self.content}')"
 
+
 def validate_input(input_data):
-    username = User.query.filter_by(username = input_data['username']).first()
-    email = User.query.filter_by(email = input_data['email']).first()
+    username = User.query.filter_by(username=input_data['username']).first()
+    email = User.query.filter_by(email=input_data['email']).first()
     if username:
         return('Username already exists.')
     elif email:
         return ('Email already used.')
-
--- app/routes.py
+++ app/routes.py
@@ -3,48 +3,88 @@ from app.models import User, Post, validate_input
 from app import app, db, bcrypt
 from flask import request, jsonify
 from flask_login import login_user, logout_user
+import uuid
 
-@app.route("/", methods = ['POST', 'GET'])
+## db.create_all()
+
+@app.route("/", methods=['POST', 'GET'])
 def home():
     return 'Home page'
 
-@app.route("/database", methods = ['POST', 'GET'])
-def database():
-    return f'Database: {User.query.all()}, {User.query.filter_by(email = "bla@live.dk").first().password}'
 
-@app.route("/register", methods = ['POST'])
+@app.route("/database", methods=['POST', 'GET'])
+def database():
+    return f'{User.query.all()}'
+
+
+@app.route("/register", methods=['POST'])
 def register():
-        
+
     if request.method == 'POST':
-        request_data = request.form
-        user_valid = validate_input(request_data)
+        user_valid = validate_input(request.form)
         if user_valid:
             return user_valid
+        unhashed_password = request.form['password']
 
-        hashed_pw = bcrypt.generate_password_hash(request_data['password'])
-        user = User(username = request_data['username'], 
-                    email = request_data['email'], 
-                    password = hashed_pw)
+        user = User(public_id=str(uuid.uuid4()),
+                    username=request.form['username'],
+                    email=request.form['email'],
+                    unhashed_password=unhashed_password)
+
         db.session.add(user)
         db.session.commit()
 
-        return jsonify([request_data['username'],
-                        request_data['email'],
-                        request_data['password']])
+        return jsonify([request.form['username'],
+                        request.form['email'],
+                        request.form['password']]
+                       )
 
-@app.route("/login", methods = ['GET', 'POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        login_data = request.form
-        user = User.query.filter_by(email = login_data['email']).first()
-        if user and bcrypt.check_password_hash(user.password, login_data['password']):
-            login_user(user, remember = True)
+        user = User.query.filter_by(email=request.form['email']).first()
+        if user and bcrypt.check_password_hash(user.password, request.form['password']):
+            login_user(user, remember=True)
             return f'Login succesful\nUser information: {User.query.get(user.id)}'
         else:
             return 'Get smashed'
 
 
-@app.route("logout", methods = ["GET", "POST"])
+@app.route("/logout", methods=["GET", "POST"])
 def logout():
     if request.method == 'POST':
         logout_user()
+
+
+@app.route("/ask", methods=['POST', 'GET'])
+def ask_question():
+    return 'bla'
+
+
+@app.route("/get_all_users", methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['username'] = user.username
+        user_data['email'] = user.email
+        output.append(user_data)
+
+    return jsonify({'users': output})
+
+@app.route("/get_one_user/<public_id>", methods=['GET'])
+def get_one_users(public_id):
+    user = User.query.filter_by(public_id = public_id).first()
+
+    if not user:
+        return jsonify({'message' : 'No user found'})
+        
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['username'] = user.username
+    user_data['email'] = user.email
+
+    return jsonify({'user': user_data})
