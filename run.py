f503e4666674a1acdd38c7e1048bd84689f24b72
Marcus Bredtved <marcushb@live.dk>
2022-04-18 16:28:25 +0200

    Removed ngrok

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-from app import app
-from app import ngrok
+from application import app
+from application import ngrok
 
-if ngrok:
-    if __name__ == '__main__':
-        app.run()
-else:
-    if __name__ == '__main__':
-        app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug = True)
