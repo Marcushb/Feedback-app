8018bdfe740810071cc868d677da93d96850498f
Julian Jensen <julian.jensen@ufst.dk>
2022-11-22 15:49:51 +0100

    readme and docoker compose update

--- application/config.py
+++ application/config.py
@@ -1,2 +1,2 @@
 secret_key = '6dce49d3b0ac1575f39720120005b5cc2ee0ed20cdba968cd2bbb345647bc9fa'
-sqlalchemy_database_uri = 'sqlite:///site.db'
+sqlalchemy_database_uri = 'postgresql+psycopg2://testuser:testpassword@postgresql-db/testuser'
